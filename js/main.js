8f95224083ee5abc1986a558684ad7d97b115539
Siggibieber <bieber1987@gmx.net>
2026-08-04 14:12:48 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,3 +43,28 @@ result.innerHTML=`
 }
 
 }
+// ==========================
+// SCROLL ANIMATION
+// ==========================
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+},{threshold:0.15});
+
+document.querySelectorAll("section").forEach(section=>{
+
+section.classList.add("hidden");
+
+observer.observe(section);
+
+});
